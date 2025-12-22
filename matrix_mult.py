14413ac40ca2ff0a851bb8fe1bcded0b7b9f235e
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-12-22 14:50:45 -0600

    implemented matrix_mult

---
+++ matrix_mult.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def matrix_multiplication(
+    A: torch.Tensor, B: torch.Tensor, dtype=torch.float32
+) -> torch.Tensor:
+    if A.ndim != 2 or B.ndim != 2:
+        raise ValueError("Either A.ndim or B.ndim != 2")
+
+    if A.shape[0] != B.shape[1]:
+        raise ValueError("A.shape[0] != B.shape[1]")
+
+    out = torch.zeros(A.shape[0], B.shape[1])
+    for i, row in enumerate(A):
+        for j, col in enumerate(B.T):
+            out[i][j] = torch.dot(row, col).item()
+    return out.type(dtype)
+
+
+A = torch.tensor([[1, 2], [3, 4]])
+print(A)
+B = torch.tensor([[5, 6], [7, 8]])
+print(B)
+
+C = matrix_multiplication(A, B, dtype=torch.int8)
+print(C)
