781399fa73c8d19100d4aef1e2125130a1a69a65
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-12-25 00:55:03 -0600

    added dataloader

--- Computer Vision/computer_vision.py
+++ Computer Vision/computer_vision.py
@@ -3,7 +3,13 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchvision import datasets
-from torchvision.transforms import ToTensor
 
-train_data = datasets.FER2013(root="Computer Vision/data", split="train")
-test_data = datasets.FER2013(root="Computer Vision/data", split="test")
+train_dataset = datasets.ImageFolder(root="Computer Vision/data/fer2013_images/train")
+test_dataset = datasets.ImageFolder(root="Computer Vision/data/fer2013_images/test")
+
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=128, shuffle=True, num_workers=4
+)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=128, shuffle=True, num_workers=4
+)
