760b1583f1e792b03837bb4dacb5a97167bdb5e2
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-12-30 19:57:17 -0600

    added the ability to have multiple metrics and changed model to follow this paper: https://cs230.stanford.edu/projects_winter_2020/reports/32610274.pdf

--- Computer Vision/computer_vision.py
+++ Computer Vision/computer_vision.py
@@ -1,8 +1,10 @@
+from typing import Dict
+
 import mlxtend
 import torch
 import torch.nn as nn
 import torchvision.transforms as T
-from torchmetrics.classification import F1Score
+from torchmetrics.classification import Accuracy, F1Score
 from torchmetrics.metric import Metric
 from torchvision import datasets
 from tqdm.auto import tqdm
@@ -28,10 +30,11 @@ def eval_model(
     model: nn.Module,
     loader: torch.utils.data.DataLoader,
     loss_fn: nn.Module,
-    metric_fn: Metric,
+    metric_fns: Dict[str, Metric],
 ):
     total_loss: float = 0.0
-    metric_fn.reset()
+    for metric_fn in metric_fns.values():
+        metric_fn.reset()
     model.eval()
     with torch.inference_mode():
         for X, y in loader:
@@ -41,8 +44,14 @@ def eval_model(
             y_pred = model(X)
 
             total_loss += loss_fn(y_pred, y).item()
-            metric_fn.update(y_pred, y)
-    return total_loss / len(loader), metric_fn.compute().item()
+            for metric_fn in metric_fns.values():
+                metric_fn.update(y_pred, y)
+
+    metrics: Dict[str, float] = {}
+    for name, metric_fn in metric_fns.items():
+        metrics[name] = metric_fn.compute().item()
+
+    return total_loss / len(loader), metrics
 
 
 def train_step(
@@ -118,59 +127,56 @@ def main():
             stride=1,
             # padding = kernel_size // 2 for odd numbered kernel sizes
             # This choice keeps input and output sizes equal
-            padding=1,
+            padding="same",
         ),
-        nn.ReLU(),
-        nn.Conv2d(
-            hidden_units,
-            hidden_units,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-        ),
+        # This layers normalizes the distribution
+        nn.BatchNorm2d(num_features=hidden_units),
         nn.ReLU(),
         nn.MaxPool2d(
             kernel_size=2,
-            # Usually stride = kernel_size so we can omit stride
+            # Usually stride = kernel_size, so we can omit stride
             stride=2,
         ),
         # Block 2
-        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
-        nn.ReLU(),
-        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+        nn.Conv2d(hidden_units, hidden_units, 3, padding="same"),
+        nn.BatchNorm2d(num_features=hidden_units),
         nn.ReLU(),
         nn.MaxPool2d(2),
         # Block 3
-        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+        nn.Conv2d(hidden_units, hidden_units, 3, padding="same"),
+        nn.BatchNorm2d(num_features=hidden_units),
         nn.ReLU(),
-        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d(2),
         # Block 4
-        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+        nn.Conv2d(hidden_units, hidden_units, 3, padding="same"),
+        nn.BatchNorm2d(num_features=hidden_units),
         nn.ReLU(),
-        nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d(2),
         # Classifier
         nn.Flatten(),
+        # Randomly zeros out some neurons to prevent overfittig
+        nn.Dropout(p=0.5),
         # In X ** 2, X = 48 * 2^(-num_max_pool_2d)
-        nn.Linear(hidden_units * 3**2, output_shape),
+        nn.Linear(hidden_units * 12**2, output_shape),
     ).to(device)
 
     loss_fn = nn.CrossEntropyLoss()
     # lr is usually 1e-3 or 1e-4 for CNN image models
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     f1_score_fn = F1Score(task="multiclass", num_classes=len(LABELS)).to(device)
+    accuracy_fn = Accuracy(task="multiclass", num_classes=len(LABELS)).to(device)
 
-    epochs = 100
+    epochs = 1
     for epoch in tqdm(range(epochs)):
         train_loss = train_step(model, train_loader, loss_fn, optimizer)
         print(f"Epoch: {epoch} | Train loss: {train_loss:.3f}\n")
 
-    print("Getting test loss and F1Score...")
-    loss, f1_score = eval_model(model, test_loader, loss_fn, f1_score_fn)
-    print(f"Test Loss: {loss} | F1Score: {f1_score}")
+    print("Getting test loss and metrics...")
+    loss, metrics = eval_model(
+        model, test_loader, loss_fn, {"F1Score": f1_score_fn, "Accuracy": accuracy_fn}
+    )
+    print(f"Test Loss: {loss:.03f}", end="  ")
+    for name, metric in metrics.items():
+        print(f"{name}: {metric * 100:.03f}%", end="  ")
+    print()
 
 
 if __name__ == "__main__":
